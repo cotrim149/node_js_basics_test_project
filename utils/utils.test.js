f1d30ded70bed1711eb1a2e09b411337f7a159ec
Victor Cotrim <cotrim149@gmail.com>
2018-08-25 16:25:11 -0300

    Adding expect library, some assertoins cases and set user test

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -1,17 +1,53 @@
 const utils = require('./utils');
+const expect = require('expect');
 
 it('Should sum two numbers', () => {
   var result = utils.add(3,7);
 
-  if (result !== 10) {
-    throw new Error(`Expected 10, but got ${result}`);
-  }
-
+  expect(result).toBe(10).toBeA('number');
 });
 
 it('Should square a number', () => {
-  var result = utils.square(2);
-  if (result !== 4) {
-    throw new Error(`Expected 4, but got ${result}`);
-  }
+  var result = utils.square(2) ;
+
+  expect(result).toBe(4, `Expected result 4, but got ${result}`).toBeA('number', `Expected a number but got ${typeof result}`);
+
+});
+
+it('Should see equality of Objects', () => {
+
+  // this always will fail
+  // expect({name: 'Cotrim'}).toBe({name: 'Cotrim'});
+
+  // For objects, must use Equals
+  expect({name: 'Cotrim'}).toEqual({name: 'Cotrim'});
+
+});
+
+it('Should see if array include or exclude some element', () => {
+  var myArray = [2,3,4];
+
+  expect(myArray).toInclude(2);
+
+  expect(myArray).toExclude(1);
+
+  var json = {
+    name: 'Cotrim',
+    age: 28
+  };
+
+  expect(json).toInclude({age: 28});
+});
+
+it('Should verify user with first and last name setted', () => {
+  var user = utils.setName({},'Victor Cotrim');
+
+  expect(user).toInclude({
+    firstName: 'Victor',
+    lastName: 'Cotrim'
+  }).toBeA('object');
+
+  expect(user.firstName).toBeA('string');
+  expect(user.lastName).toBeA('string');
+
 });

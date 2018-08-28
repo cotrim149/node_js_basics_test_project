117cd0c529d6aae86f0898ef1e25393de1b49db8
Victor Cotrim <cotrim149@gmail.com>
2018-08-27 23:28:12 -0300

    Adding my first test with real API call. Getting specific user from /users method

--- server/server.js
+++ server/server.js
@@ -3,7 +3,28 @@ const express = require('express');
 var app = express();
 
 app.get('/',(req,res)=>{
-  res.status(200).send('Hello World!');
+  res.status(404).send({
+    error: 'Page not found!'
+  });
+});
+
+app.get('/users',(req,res) => {
+  var cotrim = {
+    name: 'Cotrim',
+    age: 28
+  };
+
+  var camila = {
+    name: 'Camila',
+    age: 27
+  };
+
+  var rafael = {
+    name: 'Rafael',
+    age: 29
+  };
+
+  res.status(200).send([camila, cotrim, rafael]);
 });
 
 app.listen(3000);
--- server/server.test.js
+++ server/server.test.js
@@ -1,10 +1,29 @@
 const request = require('supertest');
+const expect = require('expect');
+
 var app = require('./server').app;
 
 it('Should return hello world response', (done) => {
   request(app)
     .get('/')
+    .expect(404)
+    .expect((res) => {
+      expect(res.body).toInclude({
+        error: 'Page not found!'
+      });
+    })
+    .end(done);
+});
+
+it('Should return specific user object', (done) => {
+  request(app)
+    .get('/users')
     .expect(200)
-    .expect('Hello World!')
+    .expect((res) => {
+      expect(res.body).toInclude({
+        name:'Cotrim',
+        age: 28
+      })
+    })
     .end(done);
 });
